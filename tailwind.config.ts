921d77bdf8fdeb1cc998840738dd8f1b88ef508a
tmcpherson13 <93959171+tmcpherson13@users.noreply.github.com>
2026-05-13 13:27:47 -0600

    Replace require() with ES imports in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
 import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
 
 export default {
   darkMode: ["class"],
@@ -103,5 +106,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+  plugins: [tailwindcssAnimate, typography],
 } satisfies Config;
